a6bd1d535a4ab729edf4a54fde8ba50c9e79ee78
hkdnet <satoko.itse@gmail.com>
2022-12-02 20:36:52 +0900

    2-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,55 @@
+enum class Hand {
+    ROCK, PAPER, SCISSORS
+}
+enum class Result {
+    WIN, DRAW, LOSE
+}
+
+val Normalized = mapOf(
+    "A" to Hand.ROCK,
+    "B" to Hand.PAPER,
+    "C" to Hand.SCISSORS,
+    "X" to Hand.ROCK,
+    "Y" to Hand.PAPER,
+    "Z" to Hand.SCISSORS
+)
+val Scores = mapOf(Hand.ROCK to 1, Hand.PAPER to 2, Hand.SCISSORS to 3);
+
+fun vs(op: Hand, me: Hand): Result {
+    if (op == me) {
+        return Result.DRAW
+    }
+    if (
+        op == Hand.ROCK && me == Hand.PAPER ||
+        op == Hand.PAPER && me == Hand.SCISSORS ||
+        op == Hand.SCISSORS && me == Hand.ROCK
+    ) {
+        return Result.WIN
+    }
+    return Result.LOSE
+}
+
 @Suppress("UNUSED_PARAMETER")
 fun main(_args: Array<String>) {
-    val elves: MutableList<List<Int>> = mutableListOf()
-    var tmp: MutableList<Int> = mutableListOf()
+    val rounds = mutableListOf<Pair<Hand, Hand>>()
     while (true) {
         val line = readLine()
-        when (line) {
-            null -> break;
-            "" -> {
-                elves.add(tmp);
-                tmp = mutableListOf()
-            }
-            else -> {
-                tmp.add(line.toInt())
-            }
+        if (line == null) {
+            break;
+        }
+        val arr = line.split(" ")
+        val h1 = Normalized.getValue(arr[0])
+        val h2 = Normalized.getValue(arr[1])
+        rounds.add(Pair(h1, h2))
+    }
+    val points = rounds.map { (opponent, mine) ->
+        val result = vs(opponent, mine)
+        when (result) {
+            Result.DRAW -> 3 + Scores.getValue(mine)
+            Result.WIN -> 6 + Scores.getValue(mine)
+            Result.LOSE -> 0 + Scores.getValue(mine)
         }
     }
-    val sorted = elves.map { it.sum() }.sortedBy{ -it }
-    val sum: Int = sorted.take(3).sum()
-    println(sum)
+
+    println(points.sum())
 }
