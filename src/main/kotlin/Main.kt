e503487156e3f26e607e1e1bc25680822e35b81c
hkdnet <satoko.itse@gmail.com>
2022-12-07 19:44:58 +0900

    6-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,44 +1,14 @@
-val moveFromTo = Regex("""^move (\d+) from (\d+) to (\d+)$""")
-
-typealias Operation = Triple<Int, Int, Int>
-
 @Suppress("UNUSED_PARAMETER")
 fun main(_args: Array<String>) {
-    var s = readln()
-    val n = (s.length + 1) / 4
-    val ds = Array<ArrayDeque<Char>>(n + 1) { ArrayDeque<Char>() }
-    while (s != "") {
-        val cs = s.toCharArray()
-        for (i in 0 until n) {
-            val idx = 4 * i + 1
-            val c = cs[idx]
-            if (c in 'A'..'Z') {
-                ds[i + 1].addFirst(c)
-            }
-        }
-        s = readln()
-    }
-    val ops = mutableListOf<Operation>()
-    while (true) {
-        val s = readlnOrNull() ?: break
-        val match = moveFromTo.matchEntire(s)!!
-        val move = match.groups[1]!!.value.toInt()
-        val from = match.groups[2]!!.value.toInt()
-        val to = match.groups[3]!!.value.toInt()
-        ops.add(Triple(move, from, to))
-    }
+    var cs = readln().toCharArray()
 
-    for ((move, from, to) in ops) {
-        val df = ds[from]
-        val dt = ds[to]
-        val tmp = mutableListOf<Char>()
-        for (c in 0 until move) {
-            tmp.add(df.removeLast())
-        }
-        for (c in tmp.reversed()) {
-            dt.addLast(c)
+    val n = 4
+    for (i in 0 until  (cs.size - n + 1) ){
+        val targets = cs.drop(i).take(n)
+        if (targets.toSet().size == n) {
+            println(i + n)
+            return
         }
     }
-    val ans = ds.drop(1).map { it.last() }.joinToString("")
-    println(ans)
+    println("ERROR!")
 }
