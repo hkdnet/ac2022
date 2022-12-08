23d4dd5e677f0f81553ac68c7db1d7628304452b
hkdnet <satoko.itse@gmail.com>
2022-12-08 14:29:00 +0900

    8-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,97 +1,56 @@
-typealias Directory = Pair<Int, List<String>>
+import java.io.File
 
+fun readLines(f: String): List<String> {
+    return File(f).useLines { it.toList() }
+}
+
+fun solve(c: List<CharArray>): Int {
+    val h = c.size
+    val w = c[0].size
+
+    var ans = 0
+
+    fun visible(x: Int, y: Int): Boolean {
+        // edge
+        if (x == 0 || y == 0 || x == h - 1 || y == w - 1) {
+            return true
+        }
+        val height = c[x][y]
+        fun leftVisible(): Boolean {
+            return (0 until x).all { c[it][y] < height }
+        }
+        fun rightVisible(): Boolean {
+            return (x+1 until h).all { c[it][y] < height }
+        }
+        fun topVisible(): Boolean {
+            return (0 until y).all { c[x][it] < height }
+        }
+        fun bottomVisible(): Boolean {
+            return (y+1 until w).all { c[x][it] < height }
+        }
+        return leftVisible() || rightVisible() || topVisible() || bottomVisible()
+    }
+
+    for (x in 0 until h) {
+        for (y in 0 until w) {
+            if (visible(x, y)) {
+                ans += 1
+            }
+        }
+    }
+
+    return ans
+}
 @Suppress("UNUSED_PARAMETER")
 fun main(_args: Array<String>) {
-    val lines = mutableListOf<String>()
-    while (true) {
-        val line = readlnOrNull() ?: break
-        lines.add(line)
+    fun parse(f: String): List<CharArray> {
+        val lines = readLines(f)
+        return lines.map { it.toCharArray() }
     }
-
-    val commands = mutableListOf<MutableList<String>>()
-    for (l in lines) {
-        if (l.startsWith("$")) {
-            val tmp = mutableListOf<String>()
-            tmp.add(l)
-            commands.add(tmp)
-        } else {
-            commands.last().add(l)
-        }
-    }
-    var cur = "/"
-    val ds: MutableMap<String, Directory> = mutableMapOf()
-    for (c in commands) {
-        val cmd = c.first()
-
-        if (cmd.startsWith("$ cd ")) {
-            val dst = cmd.drop(5)
-            when (dst) {
-                "/" -> {
-                    cur = "/"
-                }
-
-                ".." -> {
-                    cur = cur.split("/").dropLast(1).joinToString("/")
-                }
-
-                else -> {
-                    if (cur == "/") {
-                        cur = "/$dst"
-                    } else {
-                        cur += "/$dst"
-                    }
-                }
-            }
-            // println("cd-ed to $cur by $cmd")
-        } else if (cmd == "$ ls") {
-            val refs = mutableListOf<String>()
-            var size = 0
-            for (entry in c.drop(1)) {
-                if (entry.startsWith("dir ")) {
-                    val ref = entry.drop(4)
-                    if (cur == "/") {
-                        refs.add("/$ref")
-                    } else {
-                        refs.add("$cur/$ref")
-                    }
-                } else {
-                    val fileSize = entry.split(" ")[0].toInt()
-                    size += fileSize
-                }
-
-            }
-            ds[cur] = Pair(size, refs)
-        } else {
-            assert(false)
-        }
-    }
-    val memo = mutableMapOf<String, Int>()
-
-    fun calc(s: String): Int {
-        return if (memo.containsKey(s)) {
-            memo[s]!!
-        } else {
-            val (dSize, refs) = ds[s]!!
-            val size = refs.sumOf { calc(it) } + dSize
-            memo[s] = size
-            size
-        }
-    }
-    for (key in ds.keys) {
-        calc(key)
-    }
-
-    val rootSize = calc("/")
-    val total = 70_000_000
-    val threshold = 30_000_000
-    val left = total - rootSize
-    val requiredSize = threshold - left
-    println("$requiredSize is required to be deleted")
-    val sorted = memo.toList().sortedBy { p -> p.second }
-    for ((f, size) in sorted) {
-        println("%10d: $f".format(size))
-    }
-    val e = sorted.find { it.second >= requiredSize }!!
-    println(e)
-    println(e.second)
+    println(
+        solve(parse("input/8-0.txt"))
+    )
+    println(
+        solve(parse("input/8-1.txt"))
+    )
 }
