681e3889745de6ee298cc8d5ec9fb0be796c2069
hkdnet <satoko.itse@gmail.com>
2022-12-08 17:17:48 +0900

    8-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,56 +1,72 @@
 import java.io.File
 
-fun readLines(f: String): List<String> {
-    return File(f).useLines { it.toList() }
+fun readLines(): List<String> {
+    val l = mutableListOf<String>()
+    while (true) {
+        val s = readlnOrNull() ?: break
+        l.add(s)
+    }
+    return l
 }
 
 fun solve(c: List<CharArray>): Int {
     val h = c.size
     val w = c[0].size
 
-    var ans = 0
-
-    fun visible(x: Int, y: Int): Boolean {
+    fun score(x: Int, y: Int): Int {
         // edge
         if (x == 0 || y == 0 || x == h - 1 || y == w - 1) {
-            return true
+            return 0
         }
+
         val height = c[x][y]
-        fun leftVisible(): Boolean {
-            return (0 until x).all { c[it][y] < height }
-        }
-        fun rightVisible(): Boolean {
-            return (x+1 until h).all { c[it][y] < height }
-        }
-        fun topVisible(): Boolean {
-            return (0 until y).all { c[x][it] < height }
-        }
-        fun bottomVisible(): Boolean {
-            return (y+1 until w).all { c[x][it] < height }
-        }
-        return leftVisible() || rightVisible() || topVisible() || bottomVisible()
-    }
 
-    for (x in 0 until h) {
-        for (y in 0 until w) {
-            if (visible(x, y)) {
-                ans += 1
+        fun count(range: Iterable<Char>): Int {
+            var max = '0' - 1
+            var ans = 0
+            for (cc in range) {
+                if (cc >= height) {
+                    return ans + 1
+                } else {
+                    ans += 1
+                }
             }
+            return ans
         }
+
+        fun topVisibleCount(): Int {
+            return count((x - 1 downTo 0).map { c[it][y] })
+        }
+
+        fun bottomVisibleCount(): Int {
+            return count((x + 1 until h).map { c[it][y] })
+        }
+
+        fun leftVisibleCount(): Int {
+            return count((y - 1 downTo 0).map { c[x][it] })
+        }
+
+        fun rightVisibleCount(): Int {
+            return count((y + 1 until w).map { c[x][it] })
+        }
+
+        val l = leftVisibleCount()
+        val r = rightVisibleCount()
+        val t = topVisibleCount()
+        val b = bottomVisibleCount()
+        val ans = l * r * t * b
+
+        return ans
     }
 
-    return ans
+    return (0 until h).flatMap { x -> (0 until w).map { y -> Pair(x, y) } }.maxOfOrNull { (x, y) -> score(x, y) }!!
 }
+
 @Suppress("UNUSED_PARAMETER")
 fun main(_args: Array<String>) {
-    fun parse(f: String): List<CharArray> {
-        val lines = readLines(f)
+    fun parse(): List<CharArray> {
+        val lines = readLines()
         return lines.map { it.toCharArray() }
     }
-    println(
-        solve(parse("input/8-0.txt"))
-    )
-    println(
-        solve(parse("input/8-1.txt"))
-    )
+    println(solve(parse()))
 }
