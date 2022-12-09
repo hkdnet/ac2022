96227daa7638cdb78c1dde41f9f07f16a2f28415
hkdnet <satoko.itse@gmail.com>
2022-12-09 17:04:31 +0900

    9-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,64 +9,89 @@ fun readLines(): List<String> {
     return l
 }
 
-fun solve(c: List<CharArray>): Int {
-    val h = c.size
-    val w = c[0].size
+fun solve(ms: List<Move>): Int {
+    val s = mutableSetOf<Point>()
+    var h = Pair(0, 0)
+    var t = Pair(0, 0)
+    s.add(t)
 
-    fun score(x: Int, y: Int): Int {
-        // edge
-        if (x == 0 || y == 0 || x == h - 1 || y == w - 1) {
-            return 0
-        }
-
-        val height = c[x][y]
-
-        fun count(range: Iterable<Char>): Int {
-            var max = '0' - 1
-            var ans = 0
-            for (cc in range) {
-                if (cc >= height) {
-                    return ans + 1
-                } else {
-                    ans += 1
-                }
-            }
-            return ans
-        }
-
-        fun topVisibleCount(): Int {
-            return count((x - 1 downTo 0).map { c[it][y] })
-        }
-
-        fun bottomVisibleCount(): Int {
-            return count((x + 1 until h).map { c[it][y] })
-        }
-
-        fun leftVisibleCount(): Int {
-            return count((y - 1 downTo 0).map { c[x][it] })
-        }
-
-        fun rightVisibleCount(): Int {
-            return count((y + 1 until w).map { c[x][it] })
-        }
-
-        val l = leftVisibleCount()
-        val r = rightVisibleCount()
-        val t = topVisibleCount()
-        val b = bottomVisibleCount()
-        val ans = l * r * t * b
-
-        return ans
+    fun isTouching(p1: Point, p2: Point): Boolean {
+        val (x1, y1) = p1
+        val (x2, y2) = p2
+        return x2 in x1 - 1..x1 + 1 && y2 in y1 - 1..y1 + 1
     }
 
-    return (0 until h).flatMap { x -> (0 until w).map { y -> Pair(x, y) } }.maxOfOrNull { (x, y) -> score(x, y) }!!
+    fun followIfNecessary(head: Point, tail: Point): Point {
+        if (isTouching(head, tail)) {
+            return tail
+        }
+
+        val (x1, y1) = head
+        val (x2, y2) = tail
+
+        return if (x1 == x2) {
+            if (y1 < y2) {
+                Pair(x2, y2 - 1)
+            } else {
+                Pair(x2, y2 + 1)
+            }
+        } else if (y1 == y2) {
+            if (x1 < x2) {
+                Pair(x2 - 1, y2)
+            } else {
+                Pair(x2 + 1, y2)
+            }
+        } else {
+            when (Pair(x1 < x2, y1 < y2)) {
+                Pair(true, true) -> Pair(x2 - 1, y2 - 1)
+                Pair(true, false) -> Pair(x2 - 1, y2 + 1)
+                Pair(false, true) -> Pair(x2 + 1, y2 - 1)
+                Pair(false, false) -> Pair(x2 + 1, y2 + 1)
+                else -> {
+                    assert(false)
+                    Pair(0, 0)
+                }
+            }
+        }
+    }
+
+    for ((direction, step) in ms) {
+        repeat(step) {
+            h = direction.step(h)
+            t = followIfNecessary(h, t)
+            s.add(t)
+            // println("$h and $t")
+        }
+    }
+
+    return s.size
 }
+
+typealias Move = Pair<Direction, Int>
+
+enum class Direction {
+    U, D, L, R;
+
+    fun step(p: Point): Point {
+        val (x, y) = p
+        return when (this) {
+            Direction.D -> Pair(x, y - 1)
+            Direction.U -> Pair(x, y + 1)
+            Direction.L -> Pair(x - 1, y)
+            Direction.R -> Pair(x + 1, y)
+        }
+    }
+}
+typealias Point = Pair<Int, Int>
 
 @Suppress("UNUSED_PARAMETER")
 fun main(_args: Array<String>) {
-    fun parse(): List<CharArray> {
+    fun parse(): List<Move> {
         val lines = readLines()
-        return lines.map { it.toCharArray() }
+        return lines.map {
+            val a = it.split(" ")
+            Pair(Direction.valueOf(a[0]), a[1].toInt())
+        }
     }
     println(solve(parse()))
 }
