80e77cb80644b66361f862d3bcefe4fbb9f5f1a2
hkdnet <satoko.itse@gmail.com>
2022-12-20 18:22:41 +0900

    show time

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
+import java.time.Duration
+import java.time.Instant
+
 @Suppress("UNUSED_PARAMETER")
 fun main(_args: Array<String>) {
+    val startAt = Instant.now()
     Day17.exec()
+    val endAt = Instant.now()
+    println("took ${Duration.between(startAt, endAt).toMillis() / 1000.0} seconds")
 }
+
