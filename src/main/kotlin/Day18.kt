413c27dfc4710e628b5f0c6154976a4082e8268e
hkdnet <satoko.itse@gmail.com>
2022-12-21 13:09:23 +0900

    18-2 AC

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -21,109 +21,8 @@ class Day18(private val points: List<Point3>) {
             return Point3(x * v, y * v, z * v)
         }
 
-    }
-
-    enum class Axis {
-        X {
-            override val unit = Point3(1, 0, 0)
-        },
-        Y {
-            override val unit: Point3 = Point3(0, 1, 0)
-        },
-        Z {
-            override val unit: Point3 = Point3(0, 0, 1)
-        };
-
-        abstract val unit: Point3
-    }
-
-    enum class Sign {
-        Plus, Minus;
-
-        val reverse: Sign
-            get() = if (this == Plus) {
-                Minus
-            } else {
-                Plus
-            }
-    }
-
-    enum class AxisPair(private val axis: Axis, private val sign: Sign) {
-        XPlus(Axis.X, Sign.Plus),
-        XMinus(Axis.X, Sign.Minus),
-        YPlus(Axis.Y, Sign.Plus),
-        YMinus(Axis.Y, Sign.Minus),
-        ZPlus(Axis.Z, Sign.Plus),
-        ZMinus(Axis.Z, Sign.Minus);
-
-        fun goTo(dest: AxisPair): Pair<Pair<Point3, AxisPair>, Pair<Point3, AxisPair>>? {
-            if (axis == dest.axis) {
-                return null
-            }
-            var stepTo = axis.unit
-            if (sign == Sign.Minus) {
-                stepTo *= -1
-            }
-            var base = dest.axis.unit
-            if (dest.sign == Sign.Plus) {
-                base *= -1
-            }
-            return Pair(
-                Pair(base + stepTo, dest),
-                Pair(base, this)
-            )
-        }
-
-        private fun find(axis: Axis, sign: Sign): AxisPair {
-            return AxisPair.valueOf("${axis}${sign.reverse}")
-        }
-
-        val reverse: AxisPair
-            get() = find(axis, sign)
-    }
-
-    data class Face(val p: Point3, val ap: AxisPair) {
-        fun checkers(): List<Checker> {
-            val l = mutableListOf<Checker>()
-            for (toSurface in AxisPair.values()) {
-                val (firstCond, secondCond) = ap.goTo(toSurface) ?: continue
-
-                l.add(
-                    IfElseChecker(
-                        p + firstCond.first,
-                        firstCond.second,
-                        IfElseChecker(
-                            p + secondCond.first,
-                            secondCond.second,
-                            AlwaysTrueChecker(
-                                Face(p, firstCond.second.reverse)
-                            )
-                        )
-                    )
-                )
-            }
-            return l
-        }
-    }
-
-    interface Checker {
-        fun nextFace(points: Set<Point3>): Face
-    }
-
-    data class AlwaysTrueChecker(val f: Face) : Checker {
-        override fun nextFace(points: Set<Point3>): Face {
-            return f
-        }
-
-    }
-
-    data class IfElseChecker(val target: Point3, val ok: AxisPair, val elseChecker: Checker) : Checker {
-        override fun nextFace(points: Set<Point3>): Face {
-            return if (points.contains(target)) {
-                Face(target, ok)
-            } else {
-                elseChecker.nextFace(points)
-            }
+        override fun toString(): String {
+            return "(%2d, %2d, %2d)".format(x, y, z)
         }
     }
 
@@ -143,28 +42,46 @@ class Day18(private val points: List<Point3>) {
         }
     }
 
-    private fun solve(): Int {
-        val pointWithMinX = points.minByOrNull { it.x }!!
-        val visited = mutableSetOf<Face>()
+    private val minX = points.minOf { it.x }
+    private val maxX = points.maxOf { it.x }
+    private val minY = points.minOf { it.y }
+    private val maxY = points.maxOf { it.y }
+    private val minZ = points.minOf { it.z }
+    private val maxZ = points.maxOf { it.z }
 
-        val q = ArrayDeque<Face>()
-        val sFace = Face(pointWithMinX, AxisPair.XMinus)
-        q.addLast(sFace)
-        visited.add(sFace)
+    private fun solve(): Int {
+        val visited = mutableSetOf<Point3>()
+        val q = ArrayDeque<Point3>()
+
+        val init = Point3(minX - 1, minY, minZ) // this cannot be a boundary
+
+        q.addFirst(init)
+        visited.add(init)
+
+        fun isValidPoint(p: Point3): Boolean {
+            return p.x in minX-1..maxX+1 && p.y in minY-1..maxY+1 && p.z in minZ-1..maxZ+1
+        }
 
         var ans = 0
 
         while (q.isNotEmpty()) {
-            val f = q.removeFirst()
-            ans += 1
-            for (checker in f.checkers()) {
-                val nextFace = checker.nextFace(pointSet)
-                if (!visited.contains(nextFace)) {
-                    q.addLast(nextFace)
-                    visited.add(nextFace)
+            val p = q.removeFirst()
+
+            val valids = p.adjacentPoints().filter { isValidPoint(it) }
+            val nexts = valids.filter { !pointSet.contains(it) }
+
+            val diff = valids.size - nexts.size
+            if (diff > 0) {
+                ans += diff
+            }
+            for (nx in nexts) {
+                if (!visited.contains(nx)) {
+                    visited.add(nx)
+                    q.addFirst(nx)
                 }
             }
         }
+
         return ans
     }
 }
