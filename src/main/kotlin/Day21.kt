70849c8b2c6dd403bfb0c9f82b7e6dbc0a510e11
hkdnet <satoko.itse@gmail.com>
2022-12-22 12:20:54 +0900

    21-2: WA

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -1,26 +1,162 @@
+import kotlin.math.sign
+
 class Day21(private val m: Map<String, Expr>) {
     sealed interface Expr {
+        fun simplify(m: MutableMap<String, Expr>): ArithmeticExpr
         fun evaluate(m: MutableMap<String, Expr>): Long
     }
 
-    data class Constant(val v: Long) : Expr {
+    sealed interface ArithmeticExpr : Expr {
+        operator fun plus(o: ArithmeticExpr): ArithmeticExpr {
+            return BinaryOperator(this, "+", o)
+        }
+
+        operator fun minus(o: ArithmeticExpr): ArithmeticExpr {
+            return BinaryOperator(this, "-", o)
+        }
+
+        operator fun times(o: ArithmeticExpr): ArithmeticExpr {
+            return BinaryOperator(this, "*", o)
+        }
+
+        operator fun div(o: ArithmeticExpr): ArithmeticExpr {
+            return BinaryOperator(this, "/", o)
+        }
+
+        operator fun unaryMinus(): ArithmeticExpr
+    }
+
+    data class VariableHuman(val coefficient: Long) : ArithmeticExpr {
+        override fun evaluate(m: MutableMap<String, Expr>): Long {
+            return m["humn"]!!.evaluate(m)
+        }
+
+        override fun plus(o: ArithmeticExpr): ArithmeticExpr {
+            if (o is VariableHuman) {
+                return VariableHuman(coefficient + o.coefficient)
+            }
+            return super.plus(o)
+        }
+
+        override fun minus(o: ArithmeticExpr): ArithmeticExpr {
+            if (o is VariableHuman) {
+                return VariableHuman(coefficient - o.coefficient)
+            }
+            return super.minus(o)
+        }
+
+        override fun times(o: ArithmeticExpr): ArithmeticExpr {
+            if (o is Constant) {
+                return VariableHuman(coefficient * o.v)
+            }
+            return super.times(o)
+        }
+
+        override fun div(o: ArithmeticExpr): ArithmeticExpr {
+            return super.times(o)
+        }
+
+        override fun unaryMinus(): ArithmeticExpr {
+            return VariableHuman(-coefficient)
+        }
+
+        override fun simplify(m: MutableMap<String, Expr>): ArithmeticExpr {
+            return this
+        }
+
+    }
+
+    data class Constant(val v: Long) : ArithmeticExpr {
         override fun evaluate(m: MutableMap<String, Expr>): Long {
             return v
+        }
+
+        override fun plus(o: ArithmeticExpr): ArithmeticExpr {
+            if (o is Constant) {
+                return Constant(v + o.v)
+            }
+            return o + this
+        }
+
+        override fun minus(o: ArithmeticExpr): ArithmeticExpr {
+            if (o is Constant) {
+                return Constant(v - o.v)
+            }
+            return super.minus(o)
+        }
+
+        override fun times(o: ArithmeticExpr): ArithmeticExpr {
+            if (o is Constant) {
+                return Constant(v * o.v)
+            }
+            return super.times(o)
+        }
+
+        override fun div(o: ArithmeticExpr): ArithmeticExpr {
+            if (o is Constant) {
+                return Constant(v / o.v)
+            }
+            return super.div(o)
+        }
+
+        override fun simplify(m: MutableMap<String, Expr>): ArithmeticExpr {
+            return this
+        }
+
+        override fun unaryMinus(): ArithmeticExpr {
+            return Constant(-v)
         }
     }
 
     data class Reference(val name: String) : Expr {
-        override fun evaluate(m: MutableMap<String, Expr>): Long {
-            val v = m[name]!!.evaluate(m)
-            m[name] = Constant(v)
+
+        override fun simplify(m: MutableMap<String, Expr>): ArithmeticExpr {
+            val v = m[name]!!.simplify(m)
+            m[name] = v
             return v
+        }
+
+        override fun evaluate(m: MutableMap<String, Expr>): Long {
+            return m[name]!!.evaluate(m)
         }
     }
 
-    data class BinaryOperator(val l: Expr, val op: String, val r: Expr) : Expr {
+    data class BinaryOperator(val l: Expr, val op: String, val r: Expr) : ArithmeticExpr {
+        override fun unaryMinus(): ArithmeticExpr {
+            return BinaryOperator(Constant(-1L), "*", this)
+        }
+
         override fun evaluate(m: MutableMap<String, Expr>): Long {
             val lValue = l.evaluate(m)
             val rValue = r.evaluate(m)
+
+            return when (op) {
+                "+" -> {
+                    lValue + rValue
+                }
+
+                "-" -> {
+                    lValue - rValue
+                }
+
+                "*" -> {
+                    lValue * rValue
+                }
+
+                "/" -> {
+                    lValue / rValue
+                }
+
+                else -> {
+                    throw Exception("unknown operator $op")
+                }
+            }
+        }
+
+        override fun simplify(m: MutableMap<String, Expr>): ArithmeticExpr {
+            val lValue = l.simplify(m)
+            val rValue = r.simplify(m)
+
             return when (op) {
                 "+" -> {
                     lValue + rValue
@@ -61,7 +197,9 @@ class Day21(private val m: Map<String, Expr>) {
                 assert(arr.size == 2)
                 val name = arr[0]
                 val constantOp = arr[1].toLongOrNull()
-                val expr = if (constantOp == null) {
+                val expr = if (name == "humn") {
+                    VariableHuman(1)
+                } else if (constantOp == null) {
                     val binArr = arr[1].split(" ")
                     assert(binArr.size == 3)
                     BinaryOperator(
@@ -79,6 +217,46 @@ class Day21(private val m: Map<String, Expr>) {
     }
 
     private fun solve(): Long {
-        return m["root"]!!.evaluate(m.toMutableMap())
+        val root = m["root"] as BinaryOperator
+
+        val lhs = root.l
+        val rhs = root.r
+        var l = 0L
+        var r = 1_000_000_000_000_000L
+
+        fun eval(l: Long): Long {
+            val mutable = m.toMutableMap()
+            println("humn = $l")
+            mutable["humn"] = Constant(l)
+            val lv = lhs.evaluate(mutable)
+            val rv = rhs.evaluate(mutable)
+            println("$lv vs $rv")
+            return lv - rv
+        }
+        run {
+            // checking
+            val initialValueL = eval(l)
+            val initialValueR = eval(r)
+            println("$initialValueL -> ${initialValueL.sign}")
+            println("$initialValueR -> ${initialValueR.sign}")
+            assert(initialValueL.sign * initialValueR.sign < 0) {
+                "the answer is not in the range, reconsider l and r"
+            }
+        }
+        val isUpper = eval(0) < eval(1)
+        while ((r - l) > 1) {
+            val n = (r + l) / 2
+            println("$l - $r -> $n")
+            val evaluated = eval(n)
+            if (evaluated == 0L) {
+                return n
+            }
+            if (evaluated < 0 && isUpper || evaluated > 0 && !isUpper) {
+                l = n
+            } else {
+                r = n
+            }
+        }
+        TODO("unreachable!")
     }
 }
