66c95e8fb231c4c9a14bd27698e86b015514443b
hkdnet <satoko.itse@gmail.com>
2022-12-22 13:24:24 +0900

    21-2

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -3,7 +3,7 @@ import kotlin.math.sign
 class Day21(private val m: Map<String, Expr>) {
     sealed interface Expr {
         fun simplify(m: MutableMap<String, Expr>): ArithmeticExpr
-        fun evaluate(m: MutableMap<String, Expr>): Long
+        fun evaluate(m: MutableMap<String, Expr>): Pair<Long, Boolean>
     }
 
     sealed interface ArithmeticExpr : Expr {
@@ -22,89 +22,98 @@ class Day21(private val m: Map<String, Expr>) {
         operator fun div(o: ArithmeticExpr): ArithmeticExpr {
             return BinaryOperator(this, "/", o)
         }
-
-        operator fun unaryMinus(): ArithmeticExpr
+//
+//        operator fun unaryMinus(): ArithmeticExpr {
+//            return BinaryOperator(Constant(-1L), "*", this)
+//        }
     }
 
     data class VariableHuman(val coefficient: Long) : ArithmeticExpr {
-        override fun evaluate(m: MutableMap<String, Expr>): Long {
+        override fun evaluate(m: MutableMap<String, Expr>): Pair<Long, Boolean> {
             return m["humn"]!!.evaluate(m)
         }
+//
+//        override fun plus(o: ArithmeticExpr): ArithmeticExpr {
+//            if (o is VariableHuman) {
+//                return VariableHuman(coefficient + o.coefficient)
+//            }
+//            return super.plus(o)
+//        }
+//
+//        override fun minus(o: ArithmeticExpr): ArithmeticExpr {
+//            if (o is VariableHuman) {
+//                return VariableHuman(coefficient - o.coefficient)
+//            }
+//            return super.minus(o)
+//        }
+//
+//        override fun times(o: ArithmeticExpr): ArithmeticExpr {
+//            if (o is Constant) {
+//                return VariableHuman(coefficient * o.v)
+//            }
+//            return super.times(o)
+//        }
+//
+//        override fun div(o: ArithmeticExpr): ArithmeticExpr {
+//            return super.times(o)
+//        }
 
-        override fun plus(o: ArithmeticExpr): ArithmeticExpr {
-            if (o is VariableHuman) {
-                return VariableHuman(coefficient + o.coefficient)
-            }
-            return super.plus(o)
-        }
-
-        override fun minus(o: ArithmeticExpr): ArithmeticExpr {
-            if (o is VariableHuman) {
-                return VariableHuman(coefficient - o.coefficient)
-            }
-            return super.minus(o)
-        }
-
-        override fun times(o: ArithmeticExpr): ArithmeticExpr {
-            if (o is Constant) {
-                return VariableHuman(coefficient * o.v)
-            }
-            return super.times(o)
-        }
-
-        override fun div(o: ArithmeticExpr): ArithmeticExpr {
-            return super.times(o)
-        }
-
-        override fun unaryMinus(): ArithmeticExpr {
-            return VariableHuman(-coefficient)
-        }
+//        override fun unaryMinus(): ArithmeticExpr {
+//            return VariableHuman(-coefficient)
+//        }
 
         override fun simplify(m: MutableMap<String, Expr>): ArithmeticExpr {
             return this
         }
 
+        override fun toString(): String {
+            return "${coefficient}h"
+        }
     }
 
     data class Constant(val v: Long) : ArithmeticExpr {
-        override fun evaluate(m: MutableMap<String, Expr>): Long {
-            return v
+        override fun evaluate(m: MutableMap<String, Expr>): Pair<Long, Boolean> {
+            return Pair(v, true)
         }
-
-        override fun plus(o: ArithmeticExpr): ArithmeticExpr {
-            if (o is Constant) {
-                return Constant(v + o.v)
-            }
-            return o + this
-        }
-
-        override fun minus(o: ArithmeticExpr): ArithmeticExpr {
-            if (o is Constant) {
-                return Constant(v - o.v)
-            }
-            return super.minus(o)
-        }
-
-        override fun times(o: ArithmeticExpr): ArithmeticExpr {
-            if (o is Constant) {
-                return Constant(v * o.v)
-            }
-            return super.times(o)
-        }
-
-        override fun div(o: ArithmeticExpr): ArithmeticExpr {
-            if (o is Constant) {
-                return Constant(v / o.v)
-            }
-            return super.div(o)
-        }
+//
+//        override fun plus(o: ArithmeticExpr): ArithmeticExpr {
+//            if (o is Constant) {
+//                return Constant(v + o.v)
+//            }
+//            return o + this
+//        }
+//
+//        override fun minus(o: ArithmeticExpr): ArithmeticExpr {
+//            if (o is Constant) {
+//                return Constant(v - o.v)
+//            }
+//            return super.minus(o)
+//        }
+//
+//        override fun times(o: ArithmeticExpr): ArithmeticExpr {
+//            if (o is Constant) {
+//                return Constant(v * o.v)
+//            }
+//            return super.times(o)
+//        }
+//
+//        override fun div(o: ArithmeticExpr): ArithmeticExpr {
+//            if (o is Constant) {
+//                return Constant(v / o.v)
+//            }
+//            return super.div(o)
+//        }
 
         override fun simplify(m: MutableMap<String, Expr>): ArithmeticExpr {
             return this
         }
 
-        override fun unaryMinus(): ArithmeticExpr {
-            return Constant(-v)
+//        override fun unaryMinus(): ArithmeticExpr {
+//            return Constant(-v)
+//        }
+
+        override fun toString(): String {
+            return v.toString()
         }
     }
 
@@ -116,35 +125,64 @@ class Day21(private val m: Map<String, Expr>) {
             return v
         }
 
-        override fun evaluate(m: MutableMap<String, Expr>): Long {
+        override fun evaluate(m: MutableMap<String, Expr>): Pair<Long, Boolean> {
             return m[name]!!.evaluate(m)
         }
     }
 
     data class BinaryOperator(val l: Expr, val op: String, val r: Expr) : ArithmeticExpr {
-        override fun unaryMinus(): ArithmeticExpr {
-            return BinaryOperator(Constant(-1L), "*", this)
-        }
+//        override fun plus(o: ArithmeticExpr): ArithmeticExpr {
+//            if (o is Constant) {
+//                if (l is Constant) {
+//                    return BinaryOperator(l + o, op, r)
+//                }
+//                if (r is Constant) {
+//                    return BinaryOperator(l, op, r + o)
+//                }
+//            }
+//            return super.plus(o)
+//        }
+//
+//        override fun times(o: ArithmeticExpr): ArithmeticExpr {
+//            if (op == "+" || op == "-") {
+//                return BinaryOperator(
+//                    BinaryOperator(l, "*", o),
+//                    op,
+//                    BinaryOperator(r, "*", o)
+//                )
+//            }
+//            return super.times(o)
+//        }
 
-        override fun evaluate(m: MutableMap<String, Expr>): Long {
-            val lValue = l.evaluate(m)
-            val rValue = r.evaluate(m)
+//        override fun unaryMinus(): ArithmeticExpr {
+//            return BinaryOperator(Constant(-1L), "*", this)
+//        }
+
+        override fun evaluate(m: MutableMap<String, Expr>): Pair<Long, Boolean> {
+            val (lValue, lOk) = l.evaluate(m)
+            val (rValue, rOk) = r.evaluate(m)
 
             return when (op) {
                 "+" -> {
-                    lValue + rValue
+                    Pair(Math.addExact(lValue, rValue), lOk && rOk)
                 }
 
                 "-" -> {
-                    lValue - rValue
+                    Pair(Math.subtractExact(lValue, rValue), lOk && rOk)
                 }
 
                 "*" -> {
-                    lValue * rValue
+                    Pair(Math.multiplyExact(lValue, rValue), lOk && rOk)
                 }
 
                 "/" -> {
-                    lValue / rValue
+                    val v = lValue / rValue
+
+                    if (lValue % rValue == 0L) {
+                        Pair(v, lOk && rOk)
+                    } else {
+                        Pair(v, false)
+                    }
                 }
 
                 else -> {
@@ -180,8 +218,15 @@ class Day21(private val m: Map<String, Expr>) {
             }
 
         }
-    }
 
+        override fun toString(): String {
+            val base = "$l $op $r"
+            return when (op) {
+                "+", "-" -> "($base)"
+                else -> base
+            }
+        }
+    }
 
     companion object {
         fun exec() {
@@ -218,18 +263,20 @@ class Day21(private val m: Map<String, Expr>) {
 
     private fun solve(): Long {
         val root = m["root"] as BinaryOperator
+        val lhs = root.l.simplify(m.toMutableMap())
+        val rhs = root.r.simplify(m.toMutableMap())
+        val mutable = m.toMutableMap()
+        println(lhs)
+        println(rhs)
 
-        val lhs = root.l
-        val rhs = root.r
         var l = 0L
         var r = 1_000_000_000_000_000L
 
         fun eval(l: Long): Long {
-            val mutable = m.toMutableMap()
             println("humn = $l")
             mutable["humn"] = Constant(l)
-            val lv = lhs.evaluate(mutable)
-            val rv = rhs.evaluate(mutable)
+            val (lv, _) = lhs.evaluate(mutable)
+            val (rv, _) = rhs.evaluate(mutable)
             println("$lv vs $rv")
             return lv - rv
         }
@@ -243,13 +290,16 @@ class Day21(private val m: Map<String, Expr>) {
                 "the answer is not in the range, reconsider l and r"
             }
         }
-        val isUpper = eval(0) < eval(1)
+        val isUpper = eval(0) < eval(100)
+        var nearAns = 0L
         while ((r - l) > 1) {
             val n = (r + l) / 2
             println("$l - $r -> $n")
-            val evaluated = eval(n)
+
+            var evaluated = eval(n)
             if (evaluated == 0L) {
-                return n
+                nearAns = n
+                break
             }
             if (evaluated < 0 && isUpper || evaluated > 0 && !isUpper) {
                 l = n
@@ -257,6 +307,29 @@ class Day21(private val m: Map<String, Expr>) {
                 r = n
             }
         }
-        TODO("unreachable!")
+        println("Found $nearAns")
+        var delta = 0
+        while (true) {
+
+            var np = nearAns + delta
+            var nm = nearAns - delta
+            println("trying delta = $delta")
+            println("np = $np")
+            mutable["humn"] = Constant(np)
+            val (nplValue, nplOk) = lhs.evaluate(mutable)
+            val (nprValue, nprOk) = lhs.evaluate(mutable)
+            if (nplOk && nprOk && nplValue == nprValue) {
+                return np
+            }
+            println("nm = $nm")
+            mutable["humn"] = Constant(nm)
+            val (nmlValue, nmlOk) = lhs.evaluate(mutable)
+            val (nmrValue, nmrOk) = lhs.evaluate(mutable)
+            if (nmlOk && nmrOk && nmlValue == nmrValue) {
+                return nm
+            }
+
+            delta += 1
+        }
     }
 }
