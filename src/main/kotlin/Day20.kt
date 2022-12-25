5c31f0b973cf63e1eb4165bfb23211f73a3bc3ce
hkdnet <satoko.itse@gmail.com>
2022-12-25 22:38:34 +0900

    20-2 AC

--- src/main/kotlin/Day20.kt
+++ src/main/kotlin/Day20.kt
@@ -1,10 +1,10 @@
-class Day20(private val encrypted: List<Int>) {
-    class BidirectionalNode(val id: Int, val value: Int, var next: BidirectionalNode?, var prev: BidirectionalNode?) {
-        fun toList(): List<Int> {
-            val l = mutableListOf<Int>(value)
+class Day20(private val encrypted: List<Long>) {
+    class BidirectionalNode(val id: Int, val value: Long, var next: BidirectionalNode?, var prev: BidirectionalNode?) {
+        fun toList(): List<BidirectionalNode> {
+            val l = mutableListOf(this)
             var tmp = this.next!!
-            while (tmp.value != value) {
-                l.add(tmp.value)
+            while (tmp.id != id) {
+                l.add(tmp)
                 tmp = tmp.next!!
             }
             return l
@@ -20,7 +20,7 @@ class Day20(private val encrypted: List<Int>) {
         }
 
         companion object {
-            fun fromList(l: List<Int>): BidirectionalNode {
+            fun fromList(l: List<Long>): BidirectionalNode {
                 val nodes = l.mapIndexed { idx, v -> BidirectionalNode(idx, v, null, null) }
                 for (i in 0 until l.size - 1) {
                     nodes[i].linkNext(nodes[i + 1])
@@ -41,71 +41,56 @@ class Day20(private val encrypted: List<Int>) {
             println(ans)
         }
 
-        private fun parse(lines: List<String>): List<Int> {
-            return lines.map(String::toInt)
+        private fun parse(lines: List<String>): List<Long> {
+            return lines.map(String::toLong)
         }
     }
 
-    private fun solve(): Int {
+    private fun solve(): Long {
         // [-5644, 7], [-3945, 7], [7762, 6], [-2705, 6]
-        val moved = mutableSetOf<BidirectionalNode>()
-        var cur = BidirectionalNode.fromList(encrypted)
+        val head = BidirectionalNode.fromList(encrypted.map { it * 811589153L })
+        val originalPosition = head.toList()
 
         val mod = encrypted.size - 1
 
-        assert(cur.toList() == encrypted)
-        var zeroNode: BidirectionalNode? = null
-        while (moved.size < encrypted.size) {
-            println("Trying to move $cur")
-            if (moved.contains(cur)) {
-                val curNext = cur.next!!
-                println("${cur} was already moved. Trying next: ${curNext}")
-                cur = curNext
-                continue
-            }
-            var delta = cur.value
-            while (delta < 0) {
-                delta += mod
-            }
-            delta %= mod
-            // println("${cur.value} -> index delta is $delta")
-//            while (delta < 0) {
-//                delta += encrypted.size - 1
-//                println("Converted the negative delta to the positive equivalent: $delta")
-//            }
-            var target = cur
-            if (delta == 0) {
-                if (cur.value == 0) {
-                    assert(zeroNode == null)
-                    zeroNode = cur
+        val zeroNode = originalPosition.find { it.value == 0L }
+
+        println("start!")
+
+        for (round in 0 until 10) {
+            for (cur in originalPosition) {
+                // println("Trying to move $cur")
+                var delta = cur.value
+                delta %= mod
+                if (delta < 0) {
+                    delta += mod
+                }
+                assert(delta >= 0L)
+                var target = cur
+                if (delta == 0L) {
+                    continue
+                }
+                while (delta > 0) {
+                    target = target.next!!
+                    delta -= 1
                 }
 
-                moved.add(cur)
-                cur = cur.next!!
-                continue
+                // println("The target is ${target}. Insert the element next to the target.")
+                // cur prev - cur - cur next => cur prev - cur next
+                val curNext = cur.next!!
+                val curPrev = cur.prev!!
+                curPrev.linkNext(curNext)
+                // target - targetNext => target - cur - targetNext
+                val targetNext = target.next!!
+                target.linkNext(cur)
+                cur.linkNext(targetNext)
             }
-            while (delta > 0) {
-                target = target.next!!
-                delta -= 1
-            }
-
-            println("The target is ${target}. Insert the element next to the target.")
-            // cur prev - cur - cur next => cur prev - cur next
-            val curNext = cur.next!!
-            val curPrev = cur.prev!!
-            curPrev.linkNext(curNext)
-            // target - targetNext => target - cur - targetNext
-            val targetNext = target.next!!
-            target.linkNext(cur)
-            cur.linkNext(targetNext)
-
-            moved.add(cur)
-
-            cur = curNext
+            println("Round ${round + 1} finished")
         }
 
+
         var checkCur = zeroNode!!
-        var ans = 0
+        var ans = 0L
         for (i in 1..3) {
             repeat(1000) {
                 checkCur = checkCur.next!!
